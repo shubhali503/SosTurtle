f8d4fd53ba543f7dd71a61b92524a1831498e8d6
shubhali503 <shubhaliagarwal555@gmail.com>
2022-09-29 13:39:04 +0530

    final changesss

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,6 +1,5 @@
 const myHeader = document.querySelector(".header");
 window.onscroll = function () { 
-    "use strict";
     if (document.body.scrollTop >= 200 || document.documentElement.scrollTop >= 200 ) {
         myHeader.classList.add("header-colored");
         myHeader.classList.remove("header-transparent");
